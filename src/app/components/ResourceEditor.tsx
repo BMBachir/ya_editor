47f0a2259a2c724163f42b3f2e7580d8c779d120
BMBachir <boubaidjabachir3@gmail.com>
2024-07-17 19:26:21 +0100

    adding delete resourse button from outside

--- src/app/components/ResourceEditor.tsx
+++ src/app/components/ResourceEditor.tsx
@@ -1,8 +1,9 @@
 // components/ResourceEditor.tsx
 import React from "react";
 import { useYamlContext } from "./context/YamlContext";
-import { LuDelete } from "react-icons/lu";
-import { FcDeleteRow } from "react-icons/fc";
+import { MdDeleteOutline } from "react-icons/md";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 const ResourceEditor: React.FC = () => {
   const {
     jsonObjects,
@@ -23,10 +24,21 @@ const ResourceEditor: React.FC = () => {
             <h3 className="text-lg font-medium mb-2">
               Kind: {obj.kind || "Unknown"}
             </h3>
-            <button className="text-red-500 hover:text-red-600 font-semibold rounded-md shadow-sm">
-              <LuDelete className="h-5 w-5" />
-            </button>
-            <span>{expandedResourceIndex === index ? "-" : "+"}</span>
+            <div className="flex items-center justify-center gap-6 cursor-pointer">
+              <button className="text-red-500 hover:text-red-600 font-semibold rounded-md shadow-sm">
+                <MdDeleteOutline
+                  onClick={() => handleDeleteResource(index)}
+                  className="h-5 w-5"
+                />
+              </button>
+              <span>
+                {expandedResourceIndex === index ? (
+                  <IoIosArrowDown className="h-4 w-4 text-primaryColor" />
+                ) : (
+                  <IoIosArrowForward className="h-4 w-4 text-primaryColor" />
+                )}
+              </span>
+            </div>
           </div>
           {expandedResourceIndex === index && (
             <div className="rounded-md">
