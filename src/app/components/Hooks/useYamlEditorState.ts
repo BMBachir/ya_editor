785db717ac032e6807cc9c3cfb27f734b01bc0f1
BMBachir <boubaidjabachir3@gmail.com>
2024-07-19 20:53:25 +0100

    build useYaml customHook

---
+++ src/app/components/Hooks/useYamlEditorState.ts
@@ -0,0 +1,78 @@
+"use client";
+import { useState, useRef } from "react";
+import { parseAllDocuments, stringify as yamlStringify } from "yaml";
+import { parse as jsonParse, stringify as jsonStringify } from "json5";
+
+export const useYamlEditorState = () => {
+  const [yamlValue, setYamlValue] = useState<string>("");
+  const [jsonObjects, setJsonObjects] = useState<any[]>([]);
+  const [isYamlToJson, setIsYamlToJson] = useState(true);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const text = e.target?.result as string;
+        setYamlValue(text);
+        try {
+          const parsedDocuments = parseAllDocuments(text);
+          const parsedObjects = parsedDocuments.map((doc) => {
+            const obj = doc.toJSON();
+            obj.kind = obj.kind || "Unknown";
+            return obj;
+          });
+          setJsonObjects(parsedObjects);
+        } catch (error) {
+          console.error("Error parsing YAML:", error);
+        }
+      };
+      reader.readAsText(file);
+    }
+    event.target.value = "";
+  };
+
+  const handleYamlToJson = () => {
+    try {
+      const parsedDocuments = parseAllDocuments(yamlValue);
+      const parsedObjects = parsedDocuments.map((doc) => doc.toJSON());
+      const json = jsonStringify(parsedObjects, null, 2);
+      setYamlValue(json);
+      setJsonObjects(parsedObjects);
+    } catch (error) {
+      console.error("Error converting YAML to JSON:", error);
+    }
+  };
+  const handleJsonToYaml = () => {
+    try {
+      const obj = jsonParse(yamlValue);
+      const yamlString = obj
+        .map((item: any) => yamlStringify(item))
+        .join("---\n");
+      setYamlValue(yamlString);
+      setJsonObjects(obj);
+    } catch (error) {
+      console.error("Error converting JSON to YAML:", error);
+    }
+  };
+
+  const handleClearYaml = () => {
+    setYamlValue("");
+    setJsonObjects([]);
+  };
+
+  return {
+    yamlValue,
+    setYamlValue,
+    jsonObjects,
+    setJsonObjects,
+    isYamlToJson,
+    setIsYamlToJson,
+    fileInputRef,
+    handleFileChange,
+    handleYamlToJson,
+    handleJsonToYaml,
+    handleClearYaml,
+  };
+};
