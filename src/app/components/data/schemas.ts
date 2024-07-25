694a6354fe45efdf9d650580ff91a6080df057ab
BMBachir <boubaidjabachir3@gmail.com>
2024-07-25 11:22:36 +0100

    upgrade the schemas data for better mapping

--- src/app/components/data/schemas.ts
+++ src/app/components/data/schemas.ts
@@ -1,35 +1,32 @@
-// schemas.ts
-// Default schema
 export const defaultSchema = {
   name: "string",
   namespace: "string",
 };
 
-// Common schemas
-export const commonSchemas = {
-  metadata: {
-    creationTimestamp: "string",
-    labels: "object",
-    annotations: "object",
-  },
-  spec: {
-    replicas: "number",
-    selector: "object",
-    template: "object",
-  },
-};
-
-// Specific schemas for common kinds
 export const specificSchemas: { [key: string]: { [key: string]: any } } = {
   Deployment: {
     ...defaultSchema,
     replicas: "number",
     template: "object",
+    metadata: {
+      propriety: {
+        creationTimestamp: "string",
+        labels: "object",
+        annotations: "object",
+      },
+    },
   },
   Service: {
     ...defaultSchema,
     selector: "object",
     ports: "object",
+    spec: {
+      propriety: {
+        replicas: "number",
+        selector: "object",
+        template: "object",
+        imzge: "object",
+      },
+    },
   },
-  // Add other kinds as needed
 };
